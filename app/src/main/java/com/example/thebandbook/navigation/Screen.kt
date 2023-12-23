b7201168e8bdb364e4e100232914524f3b87396d
simon <simon.kp@shape.dk>
2023-12-23 21:32:23 +0100

    Add icons to data class Screen

--- app/src/main/java/com/example/thebandbook/navigation/Screen.kt
+++ app/src/main/java/com/example/thebandbook/navigation/Screen.kt
@@ -1,7 +1,11 @@
 package com.example.thebandbook.navigation
 
-sealed class Screen(val route: String) {
-    object Dashboard : Screen("dashboard")
-    object Calendar : Screen("calendar")
-    object Forum : Screen("forum")
+import androidx.annotation.DrawableRes
+import com.example.thebandbook.R
+
+sealed class Screen(val route: String, @DrawableRes val iconResourceId: Int) {
+    object Dashboard : Screen("dashboard", R.drawable.ic_dashboard)
+    object Calendar : Screen("calendar", R.drawable.ic_calendar)
+    object Forum : Screen("forum", R.drawable.ic_forum)
 }
+
