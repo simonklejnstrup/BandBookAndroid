b75479397c77d25855fa4c5a7139a0af9e8de812
simon <simon.kp@shape.dk>
2023-12-25 10:44:49 +0100

    add Event model class

---
+++ app/src/main/java/com/example/thebandbook/domain/Model/Event.kt
@@ -0,0 +1,44 @@
+package com.example.thebandbook.domain.Model
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class Event(
+    val id: Int,
+    val title: String,
+    val address: String,
+    val date: LocalDate,
+    val timeOfGetIn: LocalTime,
+    val timeOfSoundcheck: LocalTime,
+    val timeOfConcert: LocalTime,
+    val timeOfDone: LocalTime,
+    val salaryPerPerson: Int,
+    val costOfRentalGear: Int,
+    val costOfTransport: Int,
+    val extraCosts: Int,
+    val nameOfContactPerson: String,
+    val telephoneNumberOfContactPerson: String,
+    val note: String,
+    val type: String
+)
+
+enum class EventType(val displayName: String) {
+    Gig("Gig"),
+    Meeting("Meeting"),
+    Rehearsal("Rehearsal");
+
+    companion object {
+        fun fromString(type: String): EventType {
+            return when (type.lowercase()) {
+                "gig" -> Gig
+                "meeting" -> Meeting
+                "rehearsal" -> Rehearsal
+                else -> throw IllegalArgumentException("Unknown event type: $type")
+            }
+        }
+    }
+
+    fun getEventDescription(): String {
+        return "Event Type: $displayName"
+    }
+}
