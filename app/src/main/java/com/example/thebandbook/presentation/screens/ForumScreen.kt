be8d63d23d9741b539e3c002d4538a1e09721b93
simon <simon.kp@shape.dk>
2023-12-25 01:01:58 +0100

    tweeks on forum screen

--- app/src/main/java/com/example/thebandbook/presentation/screens/ForumScreen.kt
+++ app/src/main/java/com/example/thebandbook/presentation/screens/ForumScreen.kt
@@ -4,20 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -30,33 +33,91 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
-import androidx.navigation.NavController
+import com.example.thebandbook.ui.theme.TheBandBookTheme
 
 @Composable
-fun ForumScreen(navController: NavController, modifier: Modifier = Modifier) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(modifier = modifier) {
-            Text(
-                text = "Say something",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(16.dp)
-            )
+fun ForumScreen(
+//    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    TheBandBookTheme {
 
-            LazyColumn(
-                contentPadding = PaddingValues(vertical = 17.dp)
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight() // This will make the Column take up the entire available height
+                    .then(modifier), // Apply any additional modifiers passed to this composable
+                verticalArrangement = Arrangement.Center, // This centers the children vertically
+                horizontalAlignment = Alignment.CenterHorizontally // Center children horizontally as well
             ) {
-                items(count = 20) { index -> // Replace this with your data source
-                    ThreadItem(index)
+
+                Text(
+                    text = "Say something",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier.padding(30.dp)
+                )
+
+                Box {
+
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(17.dp),
+                    ) {
+                        items(count = 20) { index ->
+                            ThreadItem(index)
+                        }
+                        // Frees the last card from the bottom nav
+                        item {
+                            Spacer(modifier = Modifier.height(80.dp))
+                        }
+                    }
+                    // Gradient overlay
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color.Black.copy(alpha = 1f),
+                                        Color.Transparent
+                                    ),
+                                    startY = Float.POSITIVE_INFINITY,
+                                    endY = 0f
+                                )
+                            )
+                    )
+                    FloatingActionButton(
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(bottom = 96.dp, end = 16.dp),
+                        onClick = { /* Handle FAB click here */ },
+                        containerColor =
+                        MaterialTheme.colorScheme.secondary,
+                        //                        Color.Yellow,
+                        contentColor = MaterialTheme.colorScheme.onPrimary
+                    ) {
+                        Icon(
+                            Icons.Filled.Add,
+                            contentDescription = "Add",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+
                 }
             }
+
         }
     }
 }
@@ -115,26 +176,29 @@ fun ThreadItem(index: Int) {
             )
         }
         GrayDivider()
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(7.dp),
-                horizontalArrangement = Arrangement.End,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "17 comments",
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontWeight = FontWeight(700)
-                )
-                Icon(Icons.Default.KeyboardArrowRight, contentDescription = "See comments",
-                    tint = MaterialTheme.colorScheme.onPrimary
-                )
-            }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(7.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "17 comments",
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight(700)
+            )
+            Icon(
+                Icons.Default.KeyboardArrowRight, contentDescription = "See comments",
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+        }
     }
 }
 
 @Composable
-fun RedDivider(){
+fun RedDivider() {
     Box(
         modifier = Modifier
             .padding(horizontal = 6.dp)
@@ -145,7 +209,7 @@ fun RedDivider(){
 }
 
 @Composable
-fun GrayDivider(){
+fun GrayDivider() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -161,9 +225,10 @@ fun GrayDivider(){
 fun ThreeDotMenu() {
     var expanded by remember { mutableStateOf(false) }
 
-    Box{
+    Box {
         IconButton(onClick = { expanded = true }) {
-            Icon(Icons.Default.MoreVert, contentDescription = "More options",
+            Icon(
+                Icons.Default.MoreVert, contentDescription = "More options",
                 tint = MaterialTheme.colorScheme.onPrimary
             )
         }
@@ -195,6 +260,12 @@ fun PreviewThreeDotMenu() {
 @Composable
 fun ThreadItemPreview() {
     ThreadItem(index = 1)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ForumScreenPreview() {
+    ForumScreen()
 }
 
 
