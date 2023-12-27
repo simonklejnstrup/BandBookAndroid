0bb0c0ba8206f4d3f53279916b60bb03a9cf4c60
simon <simon.kp@shape.dk>
2023-12-27 22:17:07 +0100

    add switch for toggling

---
+++ app/src/main/java/com/example/thebandbook/util/CustomSwitch.kt
@@ -0,0 +1,47 @@
+package com.example.thebandbook.util
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomSwitch(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    // Custom dimensions
+    val trackHeight = 20.dp
+    val thumbSize = 40.dp
+    val trackWidth = 80.dp // Specify the width of the track
+
+    Box(
+        modifier = modifier
+            .width(trackWidth)
+            .height(trackHeight)
+            .clip(RoundedCornerShape(50)) // Rounded corners for the track
+            .background(if (checked) Color.Gray else Color.Gray) // Track color
+            .clickable { onCheckedChange(!checked) }
+    ) {
+        Box(
+            modifier = Modifier
+                .size(thumbSize)
+                .align(if (checked) Alignment.CenterEnd else Alignment.CenterStart)
+                .background(color = Color.DarkGray, shape = CircleShape) // Thumb styling
+        )
+    }
+}
+
