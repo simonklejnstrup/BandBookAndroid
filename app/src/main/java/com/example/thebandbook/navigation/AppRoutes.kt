9b946fb1fadc825e80425805eb4ec6c7e0ec7c1b
simon <simon.kp@shape.dk>
2023-12-27 22:15:36 +0100

    add create event screen

--- app/src/main/java/com/example/thebandbook/navigation/AppRoutes.kt
+++ app/src/main/java/com/example/thebandbook/navigation/AppRoutes.kt
@@ -3,6 +3,7 @@ package com.example.thebandbook.navigation
 object AppRoutes {
     const val DASHBOARD = "dashboard"
     const val CALENDAR = "calendar"
+    const val CREATE_EVENT_SCREEN = "create_event_screen"
     const val FORUM = "forum"
     const val FORUM_CREATE_EVENT = "forum_create_event"
 }
