5ddc11b187fea1476bafa8dbb71523abdcfedfe5
simon <simon.kp@shape.dk>
2023-12-27 22:18:28 +0100

    add custom spacers

---
+++ app/src/main/java/com/example/thebandbook/util/Spacers.kt
@@ -0,0 +1,20 @@
+package com.example.thebandbook.util
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun VSpacer(height: Dp) {
+    Spacer(modifier = Modifier.height(height).fillMaxWidth())
+}
+
+@Composable
+fun HSpacer(width: Dp) {
+    Spacer(modifier = Modifier.width(width).fillMaxHeight())
+}
