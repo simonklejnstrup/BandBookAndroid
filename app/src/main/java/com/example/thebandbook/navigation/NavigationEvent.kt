899d28e91abdb15839d92c6faf257764c1e80b0b
simon <simon.kp@shape.dk>
2023-12-29 23:55:43 +0100

    cleanup

--- app/src/main/java/com/example/thebandbook/navigation/NavigationEvent.kt
+++ app/src/main/java/com/example/thebandbook/navigation/NavigationEvent.kt
@@ -2,6 +2,5 @@ package com.example.thebandbook.navigation
 
 sealed class NavigationEvent {
     object NavigateBack: NavigationEvent()
-    data class NavigateToDetails(val id: Int): NavigationEvent()
-    // Add other navigation events as needed
+//    data class NavigateToDetails(val id: Int): NavigationEvent()
 }
