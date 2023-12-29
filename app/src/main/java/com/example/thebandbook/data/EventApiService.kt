e0a65801a815ce70131489f46ff4e51f78e201d5
simon <simon.kp@shape.dk>
2023-12-29 01:20:54 +0100

    try ip address

--- app/src/main/java/com/example/thebandbook/data/EventApiService.kt
+++ app/src/main/java/com/example/thebandbook/data/EventApiService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface EventApiService {
-    @POST("http://localhost:8080/event")
+    @POST("http://192.168.39.1:8080/event")
     suspend fun createEvent(@Body eventData: CreateEventViewModel.EventData): Response<EventResponse>
 }
