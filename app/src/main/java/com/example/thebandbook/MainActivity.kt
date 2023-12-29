dc3c8a26f7842890d3890b15e7d26177af36f668
simon <simon.kp@shape.dk>
2023-12-29 23:54:27 +0100

    cleanup

--- app/src/main/java/com/example/thebandbook/MainActivity.kt
+++ app/src/main/java/com/example/thebandbook/MainActivity.kt
@@ -11,9 +11,9 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.thebandbook.navigation.AppRoutes
-import com.example.thebandbook.presentation.screens.DashboardScreen
-import com.example.thebandbook.presentation.screens.ForumCreateThreadScreen
-import com.example.thebandbook.presentation.screens.ForumScreen
+import com.example.thebandbook.presentation.screens.dashboard.DashboardScreen
+import com.example.thebandbook.presentation.screens.forum.ForumCreateThreadScreen
+import com.example.thebandbook.presentation.screens.forum.ForumScreen
 import com.example.thebandbook.presentation.screens.calendar.CalendarScreen
 import com.example.thebandbook.navigation.BottomNavigationBar
 import com.example.thebandbook.presentation.screens.calendar.CreateEventScreen
@@ -24,28 +24,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheBandBookTheme {
                 AppContent()
-
-
-//                val navController = rememberNavController()
-//                NavHost(navController = navController, startDestination = "dashboard") {
-//                    composable(AppRoutes.DASHBOARD) { DashboardScreen() }
-//                    composable(AppRoutes.FORUM_OVERVIEW) { ForumOverviewScreen(navController) }
-//                    composable(AppRoutes.FORUM_CREATE_EVENT) { ForumCreateEventScreen(navController) }
-//                    navigation(
-//                        startDestination = "login",
-//                        route = "auth"
-//                    ) {
-//                        composable("login") {
-//                            val viewModel = it.sharedViewModel<SampleViewModel>(navController)
-//                        }
-//                        composable("register") {
-//                            val viewModel = it.sharedViewModel<SampleViewModel>(navController)
-//                        }
-//                        composable("forgot_password") {
-//                            val viewModel = it.sharedViewModel<SampleViewModel>(navController)
-//                        }
-//                    }
-//                }
             }
         }
     }
