7fecd046d093881c7b3a4215e944850c7295e3b7
simon <simon.kp@shape.dk>
2024-01-01 20:05:57 +0100

    Create custom switch for possible future impl

---
+++ app/src/main/java/com/example/thebandbook/util/CustomSwitchWithLabel.kt
@@ -0,0 +1,50 @@
+package com.example.thebandbook.util
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.thebandbook.presentation.viewmodels.CreateEventViewModel
+
+@Composable
+fun CustomSwitchWithLabel(
+    modifier: Modifier = Modifier,
+    viewModel: CreateEventViewModel
+) {
+    val onPrimary30 = MaterialTheme.colorScheme.onPrimary.copy(0.3f)
+    val onPrimary = MaterialTheme.colorScheme.onPrimary
+    Column {
+        Row {
+            Text(
+                text = "45 min",
+                // Implement correct logic
+                color = if (true) onPrimary30 else onPrimary,
+                style = MaterialTheme.typography.labelSmall
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "60 min",
+                // Implement correct logic
+                color = if (true) onPrimary else onPrimary30,
+                style = MaterialTheme.typography.labelSmall
+            )
+
+        }
+        Spacer(Modifier.height(5.dp))
+//        CustomSwitch(
+//            checked = viewModel.switchState.value,
+//            onCheckedChange = { isChecked ->
+////                viewModel.onLengthOfSetPressed(isChecked)
+////                viewModel.switchState.value = isChecked
+//            },
+//            modifier = Modifier
+//                .padding(end = 12.dp)
+//        )
+    }
+}
