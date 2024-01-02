9d4116faf07036b879bb2e9471b6de3404c08c27
simon <simon.kp@shape.dk>
2024-01-02 16:48:31 +0100

    handle navigation from dashboardscreen to sign up screen

--- app/src/main/java/com/example/thebandbook/presentation/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/thebandbook/presentation/screens/dashboard/DashboardScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -30,31 +31,46 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.thebandbook.R
 import com.example.thebandbook.data.mockEvents
 import com.example.thebandbook.data.mockThreads
 import com.example.thebandbook.domain.model.Event
 import com.example.thebandbook.domain.model.ForumThread
 import com.example.thebandbook.domain.model.User
+import com.example.thebandbook.navigation.AppRoutes
+import com.example.thebandbook.navigation.NavigationEvent
+import com.example.thebandbook.presentation.screens.authentication.SignInScreen
 import com.example.thebandbook.presentation.screens.calendar.EventItem
 import com.example.thebandbook.presentation.screens.common.BottomSheetState
+import com.example.thebandbook.presentation.screens.common.CommentInfoRow
 import com.example.thebandbook.presentation.screens.common.EventDetailBottomSheet
 import com.example.thebandbook.presentation.screens.common.ThreadBottomSheet
 import com.example.thebandbook.presentation.screens.common.ThreadBox
-import com.example.thebandbook.presentation.screens.common.CommentInfoRow
+import com.example.thebandbook.presentation.viewmodels.DashboardViewModel
 import com.example.thebandbook.presentation.viewmodels.SharedEventDetailsBottomSheetViewModel
 import com.example.thebandbook.presentation.viewmodels.SharedThreadBottomSheetViewModel
 import com.example.thebandbook.ui.theme.TheBandBookTheme
+import com.example.thebandbook.util.HSpacer
 import com.example.thebandbook.util.VSpacer
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DashboardScreen(modifier: Modifier = Modifier) {
+fun DashboardScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+
     val coroutineScope = rememberCoroutineScope()
 
     val sharedEventDetailsViewModel: SharedEventDetailsBottomSheetViewModel = viewModel()
@@ -65,7 +81,7 @@ fun DashboardScreen(modifier: Modifier = Modifier) {
     val threadBottomSheetState by sharedThreadBottomSheetViewModel.bottomSheetState.collectAsState()
     val threadModalBottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
-//    val dashboardViewModel: DashboardViewModel = viewModel()
+    val dashboardViewModel: DashboardViewModel = viewModel()
 //    val dashboardBottomSheetState by dashboardViewModel.bottomSheetState.collectAsState()
 //    val dashboardModalBottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
@@ -80,6 +96,16 @@ fun DashboardScreen(modifier: Modifier = Modifier) {
         when (threadBottomSheetState) {
             BottomSheetState.Open -> coroutineScope.launch { threadModalBottomSheetState.show() }
             BottomSheetState.Closed -> coroutineScope.launch { threadModalBottomSheetState.hide() }
+        }
+    }
+
+    // Observe navigation events
+    LaunchedEffect(key1 = Unit) {
+        dashboardViewModel.navigationEvent.collect { event ->
+            when (event) {
+                is NavigationEvent.NavigateToSignUp -> navController.navigate(AppRoutes.SIGN_UP_SCREEN)
+                else -> {}
+            }
         }
     }
 
@@ -114,25 +140,23 @@ fun DashboardScreen(modifier: Modifier = Modifier) {
                 band = "Balkan Basterds"
             )
 
-            DashboardLoggedInContent(
-                user = currentUser,
-                onSettingsClick = {
-//                    dashboardViewModel.openDashboardBottomSheet()
-                                  },
-                onThreadSelected = { forumThread ->
-                    sharedThreadBottomSheetViewModel.openBottomSheetWithThread(forumThread)
-                },
-                onEventSelected = { event ->
-                    sharedEventDetailsViewModel.openBottomSheetWithEvent(event)
-                }
-            )
 
 //        if (isLoggedIn) {
 //            DashboardLoggedInContent(
 //                user = currentUser,
-//                onSettingsClick = {})
+//                onSettingsClick = {
+////                    dashboardViewModel.openDashboardBottomSheet()
+//                                  },
+//                onThreadSelected = { forumThread ->
+//                    sharedThreadBottomSheetViewModel.openBottomSheetWithThread(forumThread)
+//                },
+//                onEventSelected = { event ->
+//                    sharedEventDetailsViewModel.openBottomSheetWithEvent(event)
+//                }
+//            )
+
 //        } else {
-//            DashboardNotLoggedInContent()
+            DashboardNotLoggedInContent(navController = navController)
 //        }
 
             // Compensate for Bottom Navigation Bar
@@ -286,13 +310,49 @@ fun DashboardHeader(
 
 
 @Composable
-fun DashboardNotLoggedInContent() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.End
-    ) {
+fun DashboardNotLoggedInContent(
+    navController: NavController
+) {
+    SignInScreen(navController)
+}
 
+
+
+
+@Composable
+fun TitleAndLogoRow() {
+    TheBandBookTheme {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "BandBook",
+                style = MaterialTheme.typography.displayLarge,
+                color = MaterialTheme.colorScheme.onPrimary)
+            HSpacer(width = 7.dp)
+            Icon(
+                painter = painterResource(id = R.drawable.logo_bandbook),
+                contentDescription = "App logo",
+                modifier = Modifier.size(50.dp),
+                tint = Color.Unspecified)
+
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun DashboardNotLoggedInContentPreview() {
+    TheBandBookTheme {
+//        val viewModel: DashboardViewModel = viewModel()
+//        val loginData = DashboardViewModel.LoginData("j", "c")
+        DashboardNotLoggedInContent(
+//            viewModel = viewModel, loginData = loginData,
+            navController = rememberNavController()
+        )
     }
 }
 
--- app/src/main/java/com/example/thebandbook/presentation/viewmodels/DashboardViewModel.kt
+++ app/src/main/java/com/example/thebandbook/presentation/viewmodels/DashboardViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.thebandbook.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.thebandbook.navigation.NavigationEvent
 import com.example.thebandbook.presentation.screens.common.BottomSheetState
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class DashboardViewModel: ViewModel() {
+
     private val _bottomSheetState = MutableStateFlow<BottomSheetState>(BottomSheetState.Open)
     val bottomSheetState = _bottomSheetState.asStateFlow()
+
+    private val _navigationEvent = MutableSharedFlow<NavigationEvent>()
+    val navigationEvent = _navigationEvent.asSharedFlow()
+
 
     fun openDashboardBottomSheet() {
         _bottomSheetState.value = BottomSheetState.Open
