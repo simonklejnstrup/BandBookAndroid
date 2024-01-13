1d2a7ea6073872e62c649e38dd9618e5b8c447f7
simon <simon.kp@shape.dk>
2024-01-13 22:19:16 +0100

    replace in project

--- app/src/main/java/com/example/thebandbook/presentation/screens/common/RedVerticalDivider.kt
+++ app/src/main/java/com/example/thebandbook/presentation/screens/common/RedVerticalDivider.kt
@@ -1,4 +1,4 @@
-package com.example.thebandbook.util
+package com.example.thebandbook.presentation.screens.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
