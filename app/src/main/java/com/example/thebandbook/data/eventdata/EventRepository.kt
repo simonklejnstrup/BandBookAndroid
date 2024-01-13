1e8c7395c0a978e267fb94795770a0363c723859
simon <simon.kp@shape.dk>
2024-01-13 22:03:57 +0100

    refactor placement and rename val

--- app/src/main/java/com/example/thebandbook/data/eventdata/EventRepository.kt
+++ app/src/main/java/com/example/thebandbook/data/eventdata/EventRepository.kt
@@ -1,11 +1,12 @@
-package com.example.thebandbook.data
+package com.example.thebandbook.data.eventdata
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val retrofit = Retrofit.Builder()
-    .baseUrl("https://your.api.url/") // Replace with your API base URL
+    .baseUrl("https://my.api.url/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
-val apiService = retrofit.create(EventApiService::class.java)
+val eventApiService = MockEventApiService() // retrofit.create(EventApiService::class.java)
+
