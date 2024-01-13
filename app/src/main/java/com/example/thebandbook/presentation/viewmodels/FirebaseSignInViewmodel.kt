c705c9274817ee04d13add7974bb099453092696
simon <simon.kp@shape.dk>
2024-01-13 22:06:09 +0100

    rename val

--- app/src/main/java/com/example/thebandbook/presentation/viewmodels/FirebaseSignInViewmodel.kt
+++ app/src/main/java/com/example/thebandbook/presentation/viewmodels/FirebaseSignInViewmodel.kt
@@ -1,26 +1,25 @@
 package com.example.thebandbook.presentation.viewmodels
 
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
-import com.example.thebandbook.presentation.firebase.sign_in.SignInResult
-import com.example.thebandbook.presentation.firebase.sign_in.SignInState
+import com.example.thebandbook.authentication.firebase.sign_in.SignInResult
+import com.example.thebandbook.authentication.firebase.sign_in.SignInState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class FirebaseSignInViewmodel: ViewModel() {
 
-    private val _state = MutableStateFlow(SignInState())
-    val state = _state.asStateFlow()
+    private val _signInState = MutableStateFlow(SignInState())
+    val signInState = _signInState.asStateFlow()
 
     fun onSingInResult(result: SignInResult) {
-        _state.update { it.copy(
+        _signInState.update { it.copy(
             isSignInSuccesful = result.data != null,
             signInErrorMessage = result.errorMessage
         ) }
     }
 
     fun resetState() {
-        _state.update { SignInState() }
+        _signInState.update { SignInState() }
     }
 }
