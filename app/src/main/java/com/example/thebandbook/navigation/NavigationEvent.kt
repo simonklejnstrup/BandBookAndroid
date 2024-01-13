b0496b2659c7ae49606bd71996614ba04e2ab4ab
simon <simon.kp@shape.dk>
2024-01-13 22:14:00 +0100

    add events to navigation

--- app/src/main/java/com/example/thebandbook/navigation/NavigationEvent.kt
+++ app/src/main/java/com/example/thebandbook/navigation/NavigationEvent.kt
@@ -3,5 +3,7 @@ package com.example.thebandbook.navigation
 sealed class NavigationEvent {
     object NavigateBack: NavigationEvent()
     object NavigateToSignUp: NavigationEvent()
+    object NavigateToCalendar: NavigationEvent()
+    object NavigateToForum: NavigationEvent()
 //    data class NavigateToDetails(val id: Int): NavigationEvent()
 }
