6bf0b5828e94c85c58dbc0c22b7a577b4039ca7f
simon <simon.kp@shape.dk>
2024-01-13 22:20:36 +0100

    add threadState and implement submitComment()

--- app/src/main/java/com/example/thebandbook/presentation/viewmodels/SharedThreadBottomSheetViewModel.kt
+++ app/src/main/java/com/example/thebandbook/presentation/viewmodels/SharedThreadBottomSheetViewModel.kt
@@ -1,10 +1,22 @@
 package com.example.thebandbook.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.thebandbook.authentication.firebase.sign_in.FirebaseUserData
+import com.example.thebandbook.data.commentdata.commentApiService
+import com.example.thebandbook.data.threaddata.mockThreads
+import com.example.thebandbook.domain.model.Comment
 import com.example.thebandbook.domain.model.ForumThread
-import com.example.thebandbook.presentation.screens.common.BottomSheetState
+import com.example.thebandbook.navigation.NavigationEvent
+import com.example.thebandbook.presentation.bottomsheets.BottomSheetState
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.time.Instant
+import kotlin.random.Random
 
 class SharedThreadBottomSheetViewModel: ViewModel() {
     private val _bottomSheetState = MutableStateFlow<BottomSheetState>(BottomSheetState.Closed)
@@ -13,8 +25,11 @@ class SharedThreadBottomSheetViewModel: ViewModel() {
     private val _selectedThread = MutableStateFlow<ForumThread?>(null)
     val selectedThread = _selectedThread.asStateFlow()
 
-    private val _textFieldValue = MutableStateFlow("")
-    val textFieldValue = _textFieldValue.asStateFlow()
+    private val _navigationEvent = MutableSharedFlow<NavigationEvent>()
+    val navigationEvent = _navigationEvent.asSharedFlow()
+
+    private val _threadState = MutableStateFlow<ForumThread?>(null)
+    val threadState: StateFlow<ForumThread?> = _threadState
 
     fun openBottomSheetWithThread(thread: ForumThread) {
         _selectedThread.value = thread
@@ -26,7 +41,44 @@ class SharedThreadBottomSheetViewModel: ViewModel() {
         _bottomSheetState.value = BottomSheetState.Closed
     }
 
-    fun submitComment(text: String, thread: ForumThread) {
+    fun submitComment(userInput: String, thread: ForumThread, currentUser: FirebaseUserData) {
+        // Find the thread in the mockThreads list
+        val foundThread = mockThreads.find { it.id == thread.id }
 
+        // Create the new comment
+        val newComment = currentUser.name?.let {name ->
+            Comment(
+                id = Random.nextInt(),
+                content = userInput,
+                createdBy = name,
+                createdAt = Instant.now(),
+                threadId = thread.id
+            )
+        }
+        newComment.let {
+            // Update the list of comments
+            val updatedComments = thread.comments + newComment!!
+            val updatedCommentsMutableList = updatedComments.toMutableList()
+
+            // Update the thread with the new list of comments
+            val updatedThread = thread.copy(comments = updatedCommentsMutableList)
+            _threadState.value = updatedThread
+        }
+
+        viewModelScope.launch {
+            try {
+                val response = commentApiService.createComment(userInput, thread, currentUser)
+                if (response.isSuccessful) {
+                    println("POST request Succesful")
+                    _navigationEvent.emit(NavigationEvent.NavigateToForum)
+                } else {
+                    println("POST request failed")
+
+                }
+            } catch (e: Exception) {
+                _navigationEvent.emit(NavigationEvent.NavigateBack)
+                println("POST request network exception ${e.message}")
+            }
+        }
     }
 }
