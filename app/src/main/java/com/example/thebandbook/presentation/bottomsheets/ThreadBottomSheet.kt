895733f10bddc741a43da1a369bd26b3010685c3
simon <simon.kp@shape.dk>
2024-01-14 20:25:01 +0100

    Optimized bottomsheet

--- app/src/main/java/com/example/thebandbook/presentation/bottomsheets/ThreadBottomSheet.kt
+++ app/src/main/java/com/example/thebandbook/presentation/bottomsheets/ThreadBottomSheet.kt
@@ -1,17 +1,20 @@
-package com.example.thebandbook.presentation.screens.common
+package com.example.thebandbook.presentation.bottomsheets
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
@@ -21,19 +24,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.thebandbook.data.mockThreads
+import com.example.thebandbook.R
+import com.example.thebandbook.authentication.GoogleAuthClientSingleton
 import com.example.thebandbook.domain.model.Comment
-import com.example.thebandbook.domain.model.ForumThread
+import com.example.thebandbook.presentation.screens.common.CommentInfoRow
+import com.example.thebandbook.presentation.screens.common.SubmitButton
 import com.example.thebandbook.presentation.viewmodels.SharedThreadBottomSheetViewModel
 import com.example.thebandbook.ui.theme.TheBandBookTheme
-import com.example.thebandbook.util.VSpacer
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -60,7 +69,6 @@ fun ThreadBottomSheet(viewModel: SharedThreadBottomSheetViewModel) {
             windowInsets = WindowInsets(0)
         ) {
             ThreadBottomSheetContent(
-                thread = selectedThread,
                 viewModel = viewModel)
         }
     }
@@ -68,37 +76,58 @@ fun ThreadBottomSheet(viewModel: SharedThreadBottomSheetViewModel) {
 
 @Composable
 fun ThreadBottomSheetContent(
-    thread: ForumThread?,
     viewModel: SharedThreadBottomSheetViewModel
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {  // Parent Column to fill the entire bottom sheet
-        thread?.let {
+    var userInput by remember { mutableStateOf("") }
+    val currentUser = GoogleAuthClientSingleton.googleAuthUiClient.getSignedInUser()
+    val thread by viewModel.selectedThread.collectAsState()
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .fillMaxHeight()
+    ) {
+        if (thread != null) {
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(1f),  // Make LazyColumn flexible in height
+                    .weight(1f),
                 verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
-                items(thread.comments) { comment ->
+                items(thread!!.comments) { comment ->
                     CommentItem(comment = comment)
                 }
             }
-        }
-
-        val textFieldValue by viewModel.textFieldValue.collectAsState()
-        OutlinedTextField(
-            value = textFieldValue,
-            onValueChange = {
-                if (thread != null) {
-                    viewModel.submitComment(it, thread)
+            OutlinedTextField(
+                value = userInput,
+                onValueChange = { userInput = it },
+                label = { Text("Add a comment") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            SubmitButton(onClick = {
+                if (currentUser != null) {
+                    thread?.let { currentThread ->
+                        viewModel.submitComment(userInput, currentThread, currentUser)
+                    }
                 }
-            },
-            label = { Text("Add a comment") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-        )
-        VSpacer(height = 50.dp)
+            })
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.CenterHorizontally)
+            ){
+                    Icon(
+                        painterResource(
+                            id = R.drawable.ic_denied),
+                            contentDescription = "Nothing found",
+                            modifier = Modifier
+                                .size(50.dp)
+                                .align(Alignment.Center)
+                    )
+            }
+        }
     }
 }
 
@@ -130,7 +159,7 @@ fun CommentItem(comment: Comment) {
 @Composable
 fun ThreadBottomSheetContentPreview() {
     val viewModel: SharedThreadBottomSheetViewModel = viewModel()
-    ThreadBottomSheetContent(mockThreads[0],viewModel )
+    ThreadBottomSheetContent(viewModel)
 }
 
 @Composable
