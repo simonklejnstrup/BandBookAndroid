7c5b5e096d7e6b897c23bd29dad0942b86476e32
simon <simon.kp@shape.dk>
2024-01-15 09:57:39 +0100

    fix navigation to ForumThreadScreen

--- app/src/main/java/com/example/thebandbook/navigation/NavigationEvent.kt
+++ app/src/main/java/com/example/thebandbook/navigation/NavigationEvent.kt
@@ -1,9 +1,10 @@
 package com.example.thebandbook.navigation
 
+
 sealed class NavigationEvent {
     object NavigateBack: NavigationEvent()
     object NavigateToSignUp: NavigationEvent()
     object NavigateToCalendar: NavigationEvent()
     object NavigateToForum: NavigationEvent()
-//    data class NavigateToDetails(val id: Int): NavigationEvent()
+    data class NavigateToForumThreadScreen(val threadId: Int): NavigationEvent()
 }
